23b0a960939e258d7126a1bed32ac2ec60fe2a7b
Kenji Oki <kenoki1990@gmail.com>
2015-08-03 17:33:18 -0500

    Se modificó GsonRequest para que acepte POST Se realizó formato base de la petición de Volley con GsonRequest. Se creó Clase User

--- app/src/main/java/app/imast/com/findingme/ui/LoginActivity.java
+++ app/src/main/java/app/imast/com/findingme/ui/LoginActivity.java
@@ -2,21 +2,50 @@ package app.imast.com.findingme.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 import app.imast.com.findingme.R;
+import app.imast.com.findingme.model.User;
+import app.imast.com.findingme.util.GsonRequest;
+import app.imast.com.findingme.util.VolleySingleton;
 
+import static app.imast.com.findingme.util.LogUtils.LOGD;
 import static app.imast.com.findingme.util.LogUtils.makeLogTag;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = makeLogTag(LoginActivity.class);
+
+    EditText edtUser, edtPass;
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtUser = (EditText) findViewById(R.id.edtUser);
+        edtPass = (EditText) findViewById(R.id.edtPass);
+
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(this);
+
+        setupToolbar();
+
     }
 
     @Override
@@ -40,4 +69,62 @@ public class LoginActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()) {
+            case R.id.btnSignIn:
+                login();
+                break;
+        }
+
+    }
+
+    //region MÉTODOS CONFIGURACIÓN
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    //endregion
+
+    //region MÉTODOS DE OPERACIÓN
+
+    private void login() {
+        String user = edtUser.getText().toString().trim();
+        String pass = edtPass.getText().toString().trim();
+
+        // Mapeo de los pares clave-valor
+        HashMap<String, String> parametros = new HashMap();
+        parametros.put("user", user);
+        parametros.put("pass", pass);
+
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(
+                new GsonRequest<User>(
+                        Request.Method.POST,
+                        "",
+                        User.class,
+                        null,
+                        new JSONObject(parametros),
+                        new Response.Listener<User>(){
+                            @Override
+                            public void onResponse(User response) {
+                                LOGD(TAG, response.toString());
+                            }
+                        },
+                        new Response.ErrorListener(){
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                LOGD(TAG, "Error Volley:"+ error.getMessage());
+                            }
+                        }
+                )
+        );
+
+    }
+
+    //endregion
+
 }
--- app/src/main/java/app/imast/com/findingme/util/GsonRequest.java
+++ app/src/main/java/app/imast/com/findingme/util/GsonRequest.java
@@ -3,11 +3,13 @@ package app.imast.com.findingme.util;
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
 import com.android.volley.ParseError;
-import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonRequest;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Map;
@@ -15,43 +17,41 @@ import java.util.Map;
 /**
  * Created by aoki on 03/08/2015.
  */
-public class GsonRequest <T> extends Request<T> {
+public class GsonRequest<T> extends JsonRequest<T> {
 
-    // Atributos
-    private final Gson gson = new Gson();
-    private final Class<T> clazz;
-    private final Map<String, String> headers;
-    private final Response.Listener<T> listener;
+    private final Gson mGson;
+    private final Class<T> mClassType;
+    private final Map<String, String> mHeaders;
+    private final Response.Listener<T> mListener;
 
-
-    /**
-     * Se predefine para el uso de peticiones GET
-     */
-    public GsonRequest(String url, Class<T> clazz, Map<String, String> headers,
+    public GsonRequest(int method, String url, Class<T> classType, JSONObject jsonRequest,
                        Response.Listener<T> listener, Response.ErrorListener errorListener) {
-        super(Method.GET, url, errorListener);
-        this.clazz = clazz;
-        this.headers = headers;
-        this.listener = listener;
+        this(method, url, classType, null, jsonRequest, listener, errorListener);
+    }
+
+    public GsonRequest(int method, String url, Class<T> classType, Map<String, String> headers,
+                       JSONObject jsonRequest, Response.Listener<T> listener,
+                       Response.ErrorListener errorListener) {
+        super(method, url, (jsonRequest == null) ? null : jsonRequest.toString(), listener,
+                errorListener);
+        mGson = new Gson();
+        mClassType = classType;
+        mHeaders = headers;
+        mListener = listener;
     }
 
     @Override
     public Map<String, String> getHeaders() throws AuthFailureError {
-        return headers != null ? headers : super.getHeaders();
+        return mHeaders != null ? mHeaders : super.getHeaders();
     }
 
     @Override
-    protected void deliverResponse(T response) {
-        listener.onResponse(response);
-    }
-
-    @Override
-    protected Response<T> parseNetworkResponse(NetworkResponse response) {
+    protected Response<T> parseNetworkResponse(NetworkResponse networkResponse) {
         try {
-            String json = new String(
-                    response.data, HttpHeaderParser.parseCharset(response.headers));
-            return Response.success(
-                    gson.fromJson(json, clazz), HttpHeaderParser.parseCacheHeaders(response));
+            String json = new String(networkResponse.data, HttpHeaderParser.parseCharset
+                    (networkResponse.headers));
+            return Response.success(mGson.fromJson(json, mClassType),
+                    HttpHeaderParser.parseCacheHeaders(networkResponse));
         } catch (UnsupportedEncodingException e) {
             return Response.error(new ParseError(e));
         } catch (JsonSyntaxException e) {
@@ -59,4 +59,8 @@ public class GsonRequest <T> extends Request<T> {
         }
     }
 
+    @Override
+    protected void deliverResponse(T response) {
+        mListener.onResponse(response);
+    }
 }
