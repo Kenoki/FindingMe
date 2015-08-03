39511f9e9fb806b779542ee1b3bf560565d3a40a
Kenji Oki <kenoki1990@gmail.com>
2015-08-03 11:19:38 -0500

    Se agregó Clase LogUtils y variables en Config

--- app/src/main/java/app/imast/com/findingme/Config.java
+++ app/src/main/java/app/imast/com/findingme/Config.java
@@ -4,4 +4,12 @@ package app.imast.com.findingme;
  * Created by aoki on 03/08/2015.
  */
 public class Config {
+
+    // Is this an internal dogfood build?
+    public static final boolean IS_DOGFOOD_BUILD = false;
+
+    // Warning messages for dogfood build
+    public static final String DOGFOOD_BUILD_WARNING_TITLE = "Test build";
+    public static final String DOGFOOD_BUILD_WARNING_TEXT = "This is a test build.";
+
 }
