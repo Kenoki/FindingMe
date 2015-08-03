74a96365d61cbf9e693397b4906aace2faa40819
Kenji Oki <kenoki1990@gmail.com>
2015-08-03 10:30:34 -0500

    Estructura base del proyecto donde se incluye las librerías a utilizar.
    - Volley
    - GSON
    - Material Design Support
    - Google Play Services

---
+++ app/src/main/java/app/imast/com/findingme/Config.java
@@ -0,0 +1,7 @@
+package app.imast.com.findingme;
+
+/**
+ * Created by aoki on 03/08/2015.
+ */
+public class Config {
+}
---
+++ app/src/main/java/app/imast/com/findingme/supportpreferenceactivity/AppCompatPreferenceActivity.java
@@ -0,0 +1,7 @@
+package app.imast.com.findingme.supportpreferenceactivity;
+
+/**
+ * Created by aoki on 03/08/2015.
+ */
+public class AppCompatPreferenceActivity {
+}
