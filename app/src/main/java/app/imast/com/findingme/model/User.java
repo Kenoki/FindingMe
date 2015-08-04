682d55e1ad996b4764403db4cc747649d1cfa711
Kenji Oki <devkenoki@gmail.com>
2015-08-04 01:05:10 -0500

    Se agregaron pantallas de Recover Password SignUP Home
    Utilizaciòn de Floating Labels

--- app/src/main/java/app/imast/com/findingme/model/User.java
+++ app/src/main/java/app/imast/com/findingme/model/User.java
@@ -1,7 +1,46 @@
 package app.imast.com.findingme.model;
 
+import java.util.Date;
+
 /**
  * Created by aoki on 03/08/2015.
  */
 public class User {
+
+    private String username;
+    private String password;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
