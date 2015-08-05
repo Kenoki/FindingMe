9131931af2f75b33d687a6672d1ec70c41f1b156
Kenji Oki <kenoki1990@gmail.com>
2015-08-05 17:33:30 -0500

    Se agregó atributos e íconos

--- app/src/main/java/app/imast/com/findingme/Config.java
+++ app/src/main/java/app/imast/com/findingme/Config.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import app.imast.com.findingme.model.District;
+import app.imast.com.findingme.model.LostPet;
+import app.imast.com.findingme.model.Pet;
 import app.imast.com.findingme.model.PetType;
+import app.imast.com.findingme.model.Race;
+import app.imast.com.findingme.model.User;
 
 /**
  * Created by aoki on 03/08/2015.
@@ -20,5 +24,10 @@ public class Config {
 
     public static List<District> lstDistrict = new ArrayList<District>();
     public static List<PetType> lstPetType = new ArrayList<PetType>();
+    public static List<Race> lstRace = new ArrayList<Race>();
+
+    public static User user;
+    public static LostPet lostPet;
+    public static Pet pet;
 
 }
